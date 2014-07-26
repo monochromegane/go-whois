08d87d3ca079623eb6b02e74ab03e9ac142607f8
monochromegane <dev.kuro.obi@gmail.com>
2014-07-26 16:14:37 +0900

    Added args number check.

--- cmd/go-whois/main.go
+++ cmd/go-whois/main.go
@@ -7,10 +7,19 @@ import (
 	"github.com/monochromegane/go-whois"
 )
 
+var usage = `usage:
+  go-whois DOMAIN_NAME`
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
 	res, err := whois.Query(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	fmt.Println(res.Raw())
 }
